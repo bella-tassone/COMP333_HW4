3cda440f78bd983cff34821fb654a618035d6fbc
Nate Levinson <natelevinson10@gmail.com>
2023-11-10 13:54:53 -0500

    added App.js in relation to previous commit

--- Frontend/react-mobile-fe/App.js
+++ Frontend/react-mobile-fe/App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 
 export default function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://172.21.219.9/index.php/rating/get');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
+      <Text>{JSON.stringify(data)}</Text>
       <StatusBar style="auto" />
     </View>
   );
