5734398c2f4836a33f9220ecfe5b34477af8f927
Nate Levinson <natelevinson10@gmail.com>
2023-11-10 14:07:22 -0500

    flatlist for future referece

--- Frontend/react-mobile-fe/App.js
+++ Frontend/react-mobile-fe/App.js
@@ -1,40 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// You can use both React and React Native.
+// We want to use Hooks, useEffect and useState, from React in our functional
+// App component. Hooks are functions that let developers "hook into" React
+// state and lifecycle features from function components.
+// In particular, we use useEffect for the fetch side-effect,
+// i.e., fetching data from a server and useState for setting a state variable
+// keeping track of whether the data to fetch is already loaded.
+import React, { useEffect, useState } from "react";
+// FlatList renders items lazily, when they are about to appear, and removes
+// items that scroll way off screen to save memory and processing time.
+import { FlatList, Text, View } from "react-native";
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-
 
 export default function App() {
+  // Initially, set isLoading to true and set up the setLoading function for
+  // later changing the isLoading value.
+  const [isLoading, setLoading] = useState(true);
+  // Initially, set data to an empty array and set up the setData function for
+  // later changing the data value to the fetched data.
+  const [ratings, setRatings] = useState([]);
 
-  const [data, setData] = useState(null);
-
+  // The useEffect hook is similar to the componentDidMount and
+  // componentDidUpdate in class components. For our anonymoust function, we will
+  // have one parameter, fetch(), and an empty function body.
+  // Note that items in a Django database can be retrieved that way as well.
+  // Try it out with Postman.
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://172.21.219.9/index.php/rating/get');
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+    axios.get(`http://172.21.44.203/index.php/rating/get?limit=100`)
+    .then((response) => {
+        setRatings(response.data);
+        setLoading(false);
+    })
+    .catch(err => console.log(err));
+}, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <Text>{JSON.stringify(data)}</Text>
-      <StatusBar style="auto" />
+    // Now the component parses the data and renders it using a FlatList component.
+    <View style={{ flex: 1, padding: 24 }}>
+      {/* As long as isLoading is true, show "Loading ..." */}
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        // Once it is false, show the fetched data.
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
+            Song List
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: "green",
+              textAlign: "center",
+              paddingBottom: 10,
+            }}
+          >
+          </Text>
+          <FlatList
+            // We use various props of the built-in FlatList component.
+            // data refers to our data; it is an array [ ].
+            // The key from the data array is extracted using the keyExtractor
+            // prop on the FlatList component.
+            // renderItem takes an item from the data and renders it on a list.
+            data={ratings}
+            renderItem={({ item }) => (
+              <Text>{item.id + ". " + item.song}</Text>
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
