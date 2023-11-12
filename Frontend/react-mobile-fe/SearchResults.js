4752809ee32a7c8560db7630f405ebcfbb6f47a1
bella.tassone <itassone@wesleyan.edu>
2023-11-12 10:03:43 -0500

    account for no results found case #13

--- Frontend/react-mobile-fe/SearchResults.js
+++ Frontend/react-mobile-fe/SearchResults.js
@@ -27,7 +27,7 @@ export default function SearchResults({navigation, route}) {
           style={{
             flex: 1,
             flexDirection: "column",
-            justifyContent: "space-between"
+            justifyContent: "flex-start"
           }}
         >
           <View style={{alignItems:'flex-end'}}>
@@ -35,24 +35,28 @@ export default function SearchResults({navigation, route}) {
               <Text style={styles.buttonText}>Home</Text>
             </Pressable>
           </View>
-          <Text style={{ fontSize: 30, color: "grey", textAlign: "center", marginTop:10}}>
+          <Text style={{ fontSize: 30, color: "grey", textAlign: "center", marginTop:10, textDecorationLine:'underline'}}>
             {"Search results for '" + route.params.search + "'"}
           </Text>
-          <FlatList
-            style={{margin:12}}
-            data={users}
-            renderItem={({ item }) => (
-              <TouchableOpacity 
-                    key={item.username}
-                    style={styles.button} 
-                    onPress={() => navigation.navigate("User Details", {user: item.username})}
-                >
-                <View style={{flexDirection: 'row', justifyContent: "center", alignItems: "center"}}>
-                  <Text style={styles.userText}>{'@' + item.username}</Text>
-                </View>
-              </TouchableOpacity>
-            )}
-          />
+          {(Object.keys(users).length != 0) ? (
+            <FlatList
+                style={{margin:12}}
+                data={users}
+                renderItem={({ item }) => (
+                <TouchableOpacity 
+                        key={item.username}
+                        style={styles.button} 
+                        onPress={() => navigation.navigate("User Details", {user: item.username})}
+                    >
+                    <View style={{flexDirection: 'row', justifyContent: "center", alignItems: "center"}}>
+                    <Text style={styles.userText}>{'@' + item.username}</Text>
+                    </View>
+                </TouchableOpacity>
+                )}
+            />
+            ) : (
+            <Text style={{textAlign:'center', marginTop:20, fontSize:20, color:'grey'}}>No results found.</Text>
+          )}
         </View>
       )}
     </View>
