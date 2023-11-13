64c006de99a022401c3b70431846b9a7b01aba5a
Nate Levinson <natelevinson10@gmail.com>
2023-11-13 10:17:18 -0500

    clean up code (issue-#03)

--- Frontend/react-mobile-fe/DeleteRating.js
+++ Frontend/react-mobile-fe/DeleteRating.js
@@ -9,7 +9,7 @@ export default function DeleteRating({ navigation, route }) {
   useEffect(() => {
     // Make API call to delete the rating
     axios.delete(`http://172.21.219.9/index.php/rating/delete?id=${idToDelete}`, {
-      data: { username: user } // Send the username in the request body
+      data: { username: user }
     })
     .then(response => {
       Alert.alert("Success", "Rating deleted successfully");
